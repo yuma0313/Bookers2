f2ee976bd9f2789a8a590e76a756dbb47858bb78
yuma0313 <y.yuma0313@gmail.com>
2021-08-01 12:01:18 +0000

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :followings, through: :relationships, source: :followed
+  
   # ユーザーをフォローする
   def follow(user_id)
     relationships.create(followed_id: user_id)
